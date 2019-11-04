04703e0668907589d29f245f990aef3e63104423
shinework <adrien.baptiste@gmail.com>
2019-11-04 20:25:34 +0100

    init react app front

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div style={{ fontSize: 40 }}>reactivated.app ⚡️</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
