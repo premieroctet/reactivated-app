a2a355d425069bf71a7cb50a0402f0840572628c
Pierre <pierre.crbs@gmail.com>
2019-11-07 12:28:18 +0100

    add swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Reactived App')
+    .setDescription('Reactived App description')
+    .setVersion('1.0')
+    .addTag('reactived app')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
