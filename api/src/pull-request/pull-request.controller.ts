f9a3574ef25e5557a19c6ce4f3e2eb090fae3ba5
thozh <thomaszhou974@gmail.com>
2020-06-08 09:25:11 +0200

    query join repositories for pull req

---
+++ api/src/pull-request/pull-request.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Logger, Get } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { PullRequest } from './pull-request.entity';
+import { PullRequestService } from './pull-request.service';
+
+@Crud({
+  model: { type: PullRequest },
+
+  query: {
+    join: {
+      repository: {
+        eager: true,
+      },
+    },
+  },
+})
+@Controller('pull-requests')
+export class PullRequestController implements CrudController<PullRequest> {
+  private readonly logger = new Logger(this.constructor.name);
+
+  constructor(public service: PullRequestService) {}
+}
--- api/src/pull-request/pull-request.module.ts
+++ api/src/pull-request/pull-request.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PullRequest } from './pull-request.entity';
 import { PullRequestService } from './pull-request.service';
+import { PullRequestController } from './pull-request.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PullRequest])],
   providers: [PullRequestService],
   exports: [PullRequestService],
+  controllers: [PullRequestController],
 })
 export class PullRequestModule {}
