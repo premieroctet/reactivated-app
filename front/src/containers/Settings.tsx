a0e5b83c14a8529b4de806fecf60a2ee10201481
thozh <thomaszhou974@gmail.com>
2020-06-17 09:36:33 +0200

    validate delete account

--- front/src/containers/Settings.tsx
+++ front/src/containers/Settings.tsx
@@ -1,16 +1,35 @@
-import { Button, Text } from '@chakra-ui/core'
+import {
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+} from '@chakra-ui/core'
+import { deleteFromStorage } from '@rehooks/local-storage'
 import React from 'react'
 import { FaTrash } from 'react-icons/fa'
-import Container from '../components/Container'
-import { useAuth } from '../contexts/AuthContext'
 import { deleteAccount } from '../api/user'
+import Container from '../components/Container'
+import { Row } from '../components/Flex'
+import { useAuth } from '../contexts/AuthContext'
+
 const Settings = () => {
   const { jwTokenData } = useAuth()
   const [deleteAccountSuccess, setDeleteAccountSuccess] = React.useState(false)
+
   const onDeleteAccount = async () => {
-    const res = await deleteAccount(jwTokenData?.userId)
-    if (res.status === 201) {
+    try {
+      await deleteAccount(jwTokenData?.userId)
       setDeleteAccountSuccess(true)
+      setTimeout(() => {
+        deleteFromStorage('token')
+      }, 800)
+    } catch (error) {
+      console.error(error)
     }
   }
 
@@ -21,14 +40,28 @@ const Settings = () => {
       </Container>
 
       <Container>
-        <Button
-          leftIcon={FaTrash}
-          variantColor="red"
-          variant="outline"
-          onClick={onDeleteAccount}
-        >
-          Delete my account
-        </Button>
+        <Popover>
+          <PopoverTrigger>
+            <Button leftIcon={FaTrash} variantColor="red" variant="outline">
+              Delete my account
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent zIndex={4}>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverHeader>
+              <Text color="red.500">Confirm deletion</Text>
+            </PopoverHeader>
+            <PopoverBody>
+              Are you sure to delete your account?
+              <Row justify="center" mt={5}>
+                <Button variantColor="red" onClick={onDeleteAccount}>
+                  Confirm
+                </Button>
+              </Row>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
 
         {deleteAccountSuccess && (
           <Text color="green.300">Your account has been correctly deleted</Text>
